64121cb100d0777c4f2e5886803b7071c7a6b1bf
jhge <jhge@sjtu.edu.cn>
2021-12-08 15:15:28 +0800

    前言样式调整

--- src/main/ui/src/views/preface/index.vue
+++ src/main/ui/src/views/preface/index.vue
@@ -2,9 +2,8 @@
     <div class="mobile-page preface-c">
         <div class="content">
             <div class="title">前言</div>
-            <div class="part">上海商学院深入学习贯彻党的十九届六中全会精神，在推进党史学习教育中发挥商科学研优势，践行和弘扬伟大建党精神，坚持用活上海作为党的“初心之地”的红色资源，聚焦商业博物馆的“红色富矿”，遴选荣誉教师和思政课教师代表带领学生开展实景互动体验式教学，进一步发挥课堂主渠道育人功能，形成寻访商业博物馆红色基因主题党史学习教育系列微课。
-                学校党委坚持通过开展红色寻访，让党史学习教育更有感染力；讲授红色故事，让党史学习教育更有穿透力；传承红色基因，让党史学习教育更有影响力。</div>
-            <div class="part">学校党委号召广大师生在伟大建党精神激励下，以史为鉴，开创未来，继续秉持“厚德博学 经世济民”校训精神，一如既往抢抓机遇、攻坚克难，爬坡奋进、开拓创新，为早日建成特色鲜明的高水平应用型商科大学而努力奋斗！</div>
+            <div class="part">上海商学院深入学习贯彻党的十九届六中全会精神，在推进党史学习教育中发挥商科学研优势，践行和弘扬伟大建党精神，坚持用活上海作为党的“初心之地”的红色资源，聚焦商业博物馆的“红色富矿”，遴选荣誉教师和思政课教师代表带领学生开展实景互动体验式教学，进一步发挥课堂主渠道育人功能，形成寻访商业博物馆红色基因主题党史学习教育系列微课。</div>
+            <div class="part">学校党委坚持通过开展红色寻访，让党史学习教育更有感染力；讲授红色故事，让党史学习教育更有穿透力；传承红色基因，让党史学习教育更有影响力。学校党委号召广大师生在伟大建党精神激励下，以史为鉴，开创未来，继续秉持“厚德博学 经世济民”校训精神，一如既往抢抓机遇、攻坚克难，爬坡奋进、开拓创新，为早日建成特色鲜明的高水平应用型商科大学而努力奋斗！</div>
         </div>
         <img src="../../assets/preface/footer.png" class="footer-img">
         <div class="arrow-c" @click="handleGo">
@@ -33,11 +32,12 @@
 
     .content {
         flex: 1;
-        padding-top: 35.4%;
+        padding-top: 34%;
         color: #FFF;
     }
 
     .title {
+        padding-bottom: 8px;
         font-size: 24px;
         font-weight: 600;
         letter-spacing: 2px;
@@ -46,8 +46,8 @@
 
     .part {
         line-height: 22px;
-        padding: 16px 36px 0;
-        font-size: 13px;
+        padding: 12px 36px 0;
+        font-size: 14px;
         text-align: justify;
         text-indent: 2em;
     }
@@ -57,17 +57,19 @@
     }
 
     .arrow-c {
-        line-height: 80px;
+        padding: 0 2px;
+        line-height: 120px;
         position: absolute;
         right: 8px;
-        top: calc(50% - 40px);
+        top: calc(50% - 60px);
         cursor: pointer;
         animation: handle 0.5s 1 ;
         animation-iteration-count: infinite;
         animation-direction: alternate;
 
         .arrow {
-            width: 20px;
+            width: 18px;
+            height: 22px;
         }
 
     }
